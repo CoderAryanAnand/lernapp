34b53c987683b5303757353200ffd4efa3929659
Aryan Anand <aryananand.chess@gmail.com>
2025-03-26 14:46:23 +0100

    make email unique

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
 
 # Ensure database tables are created
 with app.app_context():
