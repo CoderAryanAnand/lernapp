a9f54c7b1d157fe32dd19df5f7c6c4329b33fb08
Aryan Anand <aryananand.chess@gmail.com>
2025-10-10 23:53:33 +0200

    ssl instead of tls

--- app.py
+++ app.py
@@ -37,9 +37,9 @@ app.secret_key = os.getenv("SECRET_KEY")  # Secret key for session management an
 
 # Mail configuration (for password reset emails)
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
-app.config["MAIL_PORT"] = 587
-app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USE_SSL"] = False
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_TLS"] = False
+app.config["MAIL_USE_SSL"] = True
 app.config["MAIL_USERNAME"] = "kantikoala@gmail.com"
 app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
 
