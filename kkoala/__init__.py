3a0c3a6a0ff779896d30270d224163afdc5e08c4
Aryan Anand <aryananand.chess@gmail.com>
2025-11-02 16:39:38 +0100

    error pages

--- kkoala/__init__.py
+++ kkoala/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from dotenv import load_dotenv
 import resend
 
@@ -21,6 +21,20 @@ def create_app(config_class="config.ProdConfig"):
         resend.api_key = resend_key
 
     app.before_request(make_csrf_token)
+
+    @app.errorhandler(404)
+    def not_found_error(error):
+        return render_template('404.html'), 404
+
+    @app.errorhandler(500)
+    def internal_error(error):
+        # It's good practice to roll back the session in case of a server error
+        db.session.rollback() 
+        return render_template('500.html'), 500
+
+    @app.errorhandler(403)
+    def forbidden_error(error):
+        return render_template('403.html'), 403
 
     register_blueprints(app)
 
